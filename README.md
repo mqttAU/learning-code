e469d3a3532cfbce2fefeb39e4d51b4689ffc2d1
m <matttran2002@gmail.com>
2022-11-17 22:05:11 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # learning-code
  practise coding, refreshing knowledge and practising a range of leetcode questions and different languages.
+
+
+ Resources:
+
+ Leetcode:
+ https://www.codility.com/
+ https://www.hackerrank.com/
+ https://leetcode.com/
+
+ GoLang:
+
+ Java:
+
+ Python:
+
+ C:
+
+ C#:
+
+ SQL:
+
+ PHP:
+
+ React.js:
---
+++ learning-python/vars & datatypes.py
@@ -0,0 +1,14 @@
+
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + ", ")
+print("he was 35 years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
+# THIS IS THE PRINT STATEMENTS WITHOUT THE VARIABLES IMPLEMENTED
+# print("There once was a man named John, ")
+# print("he was 35 years old. ")
+# print("He really liked the name John, ")
+# print("but didn't like being 35.")
